0eaa80e5ca07bc7adfc855ad9d26677df3d25d7d
AleixFE <127050923+AleixFE@users.noreply.github.com>
2023-03-11 12:05:24 +0100

    File Loader sin pulir

--- Project_1/FileLoader.Designer.cs
+++ Project_1/FileLoader.Designer.cs
@@ -28,19 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconButton1.IconColor = System.Drawing.Color.Black;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.Location = new System.Drawing.Point(12, 12);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(140, 50);
+            this.iconButton1.TabIndex = 0;
+            this.iconButton1.Text = "iconButton1";
+            this.iconButton1.UseVisualStyleBackColor = true;
+            this.iconButton1.Click += new System.EventHandler(this.iconButton1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(158, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(134, 20);
+            this.textBox1.TabIndex = 1;
             // 
             // FileLoader
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2194, 1182);
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(722, 445);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.iconButton1);
+            this.Margin = new System.Windows.Forms.Padding(1);
             this.Name = "FileLoader";
             this.Text = "Load a File";
+            this.Load += new System.EventHandler(this.FileLoader_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Project_1/FileLoader.cs
+++ Project_1/FileLoader.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FontAwesome.Sharp;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,37 @@ namespace Project_1
 {
     public partial class FileLoader : Form
     {
+        private const string AstFileFilter = "Archivos AST (*.ast)|*.ast";
+        
+
         public FileLoader()
         {
             InitializeComponent();
+            openFileDialog1.Filter = AstFileFilter;
+        }
+
+        private void FileLoader_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            openFileDialog.Filter = AstFileFilter;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var filePath = openFileDialog.FileName;
+                textBox1.Text = filePath;
+
+                // Llama al método para decodificar el archivo .ast
+                AsterixDecoder(filePath);
+            }
+        }
+        private static void AsterixDecoder(string filePath)
+        {
+            // decodificar el archivo .ast
         }
     }
 }
